c9bb375a26ca9bf0553206a10d3e5c8a2fb7a139
Nabi <46531267+nabi1020-dev@users.noreply.github.com>
2022-07-26 23:17:11 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { ReactDOM} from "react";  
+import React, { ReactDOM} from "react";   
 import { BrowserRouter as Router,Routes, Route } from "react-router-dom";
 
 import Header from "./components/Header";
